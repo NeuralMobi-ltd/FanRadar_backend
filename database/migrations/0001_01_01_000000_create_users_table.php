b88b6e0cfcbf65ee667064e6182019ba171606a6
Ellouzati-Mohammed <MohammedEllouzati047@gmail.com>
2025-08-31 18:05:25 +0100

    profile_image constaint bug fix

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email')->unique();
-            $table->string('profile_image');
+            $table->string('profile_image')->nullable();;
             $table->enum('role', ['user', 'admin'])->default('user');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
